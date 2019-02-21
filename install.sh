d38646bed8e1932c75e770aa56008c4323c65143
Ruben Tsirunyan <rubentsirunyan@gmail.com>
2019-02-21 14:10:03 +0400

    Minor fixes

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git submodule update --init --recursive
+
 sudo apt install -y terminator
 sudo apt install -y vim-gtk
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
@@ -8,3 +10,4 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 cp -r .vim* ~/
 cp -r terminator ~/
 
+vim +PluginInstall +qall
