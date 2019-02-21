26739fe716712760da93c1cbed35d54da5b83d88
Ruben Tsirunyan <rubentsirunyan@gmail.com>
2019-02-21 13:56:13 +0400

    Minor fixes

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ sudo apt install -y vim-gtk
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all --no-zsh --no-fish
 
-cp .vim* ~/
+cp -r .vim* ~/
 cp -r terminaror ~/
 
