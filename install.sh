01ba8a7a4a01a0b96e7f8caaaa76263f5cb9966a
Ruben Tsirunyan <rubentsirunyan@gmail.com>
2019-02-21 15:05:01 +0400

    Adding vundle install

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-git submodule update --init --recursive
+
 
 sudo apt install -y terminator
 sudo apt install -y vim-gtk
@@ -10,6 +10,7 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 cp -r .vim* ~/
 cp -r terminator ~/.config/
 
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
 if ! [ -f /etc/bashrc ]; then
