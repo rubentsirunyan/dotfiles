9e954ef7ac33a0330c2c23c59300b825d044be37
Ruben Tsirunyan <rubentsirunyan@gmail.com>
2019-02-21 13:53:02 +0400

    Adding an install script

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt install -y terminator
+sudo apt install -y vim-gtk
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install --all --no-zsh --no-fish
+
+cp .vim* ~/
+cp -r terminaror ~/
+
