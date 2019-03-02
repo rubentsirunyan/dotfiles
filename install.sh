db18b7c42df55ff3d616cd79c2e9acf5072a9963
Ruben Tsirunyan <rubentsirunyan@gmail.com>
2019-03-02 23:34:43 +0400

    Updating the install script

--- install.sh
+++ install.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-
-
 sudo apt install -y terminator
 sudo apt install -y vim-gtk
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all --no-zsh --no-fish
+
+# Install the themes
+sudo add-apt-repository -y ppa:ryu0/aesthetics
+sudo add-apt-repository -y ppa:papirus/papirus
+sudo add-apt-repository -y ppa:tista/adapta
+sudo apt update
+sudo apt install -y matcha-theme adapta-gtk-theme papirus-icon-theme
+
+# Docky
+sudo apt install -y docky
 
 cp -r .vim* ~/
 cp -r terminator ~/.config/
