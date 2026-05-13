93f2f4f1a0f727b09200679762cf7d4cb452bea7
Ruben Tsirunyan <rubentsirunyan@gmail.com>
2026-05-13 21:42:39 +0400

    Blink keymaps

--- .config/nvim/lua/config/plugins/blink.lua
+++ .config/nvim/lua/config/plugins/blink.lua
@@ -1,7 +1,11 @@
 require('blink.cmp').setup({
-  keymap = { preset = 'default' },
+  keymap = {
+    preset = 'default',
+    ['<Tab>'] = { 'select_next', 'snippet_forward', 'fallback' },
+    ['<S-Tab>'] = { 'select_prev', 'snippet_forward', 'fallback' },
+  },
   completion = {
-    list = { selection = { preselect = false, auto_insert = false } },
+    list = { selection = { preselect = false, auto_insert = true } },
     menu = { auto_show = true },
     documentation = { auto_show = true, auto_show_delay_ms = 200 },
   },
--- .config/nvim/lua/config/plugins/diffview.lua
+++ .config/nvim/lua/config/plugins/diffview.lua
@@ -1,1 +1,16 @@
-require('diffview').setup({})
+require('diffview').setup({
+  enhanced_diff_hl = true,
+  view = {
+    default = {
+      winbar_info = true,
+    },
+    merge_tool = {
+      layout = 'diff4_mixed',
+      disable_diagnostics = true,
+      winbar_info = true,
+    },
+    file_history = {
+      winbar_info = true,
+    },
+  },
+})
